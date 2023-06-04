5b254b7c685a07368f70330d23b1cc4d93065cc6
Rumen Nikolov <rumenhco2001@abv.bg>
2023-06-04 11:19:48 +0300

    ex6 added

---
+++ src/OpinionPoll_01/Person.java
@@ -0,0 +1,28 @@
+package OpinionPoll_01;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        //връща обектът под формата на текст
+        //обект = тескт "{name} - {age}"
+        return name + " - " + age;
+    }
+
+}
