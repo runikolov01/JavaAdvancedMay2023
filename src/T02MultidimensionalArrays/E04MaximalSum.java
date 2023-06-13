8358b1026ed3aa3c2c9cac23f0dd1bc7c020f35d
Rumen Nikolov <rumenhco2001@abv.bg>
2023-06-13 18:11:31 +0300

    Multidimensional Arrays - Exercises

--- src/T02MultidimensionalArrays/E04MaximalSum.java
+++ src/T02MultidimensionalArrays/E04MaximalSum.java
@@ -1,30 +1,60 @@
 package T02MultidimensionalArrays;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class E04MaximalSum {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int rows = scan.nextInt();
-        int cols = scan.nextInt();
-        int[][] matrix = new int[rows][cols];
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        if (rows >= 3 && cols >= 3) {
-            fillMatrix(matrix, rows, cols, scan);
+        int[][] matrix = fillMatrix(reader);
 
-        }
-    }
+        int maxSum = Integer.MIN_VALUE;
+        int row = 0;
+        int col = 0;
+        for (int rows = 0; rows < matrix.length-2; rows++) {
+            for (int cols = 0; cols < matrix[0].length-2; cols++) {
+                int sum = 0;
+                sum += matrix[rows][cols] + matrix[rows][cols+1] + matrix[rows][cols+2];
+                sum += matrix[rows+1][cols] + matrix[rows+1][cols+1] + matrix[rows+1][cols+2];
+                sum += matrix[rows+2][cols] + matrix[rows+2][cols+1] + matrix[rows+2][cols+2];
 
-    private static void fillMatrix(int[][] matrix, int rows, int cols, Scanner scan) {
-        int counter = 0;
-        for (int r = 0; r < rows; r++) {
-            int[] inpArr = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
-            for (int c = 0; c < cols; c++) {
-                int curEl = inpArr[counter];
-                matrix[r][c] = curEl;
-                counter++;
+                if (sum > maxSum){
+                    maxSum = sum;
+                    row = rows;
+                    col = cols;
+                }
             }
         }
+
+
+        System.out.println("Sum = " + maxSum);
+        for (int i = row; i < row+3; i++) {
+            for (int j = col; j < col+3; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
+
+    }
+
+    private static int[][] fillMatrix(BufferedReader reader) throws IOException {
+        String[] rowsCols = reader.readLine().split(" ");
+        int rows = Integer.parseInt(rowsCols[0]);
+        int cols = Integer.parseInt(rowsCols[1]);
+
+        int[][] matrix = new int[rows][cols];
+        for (int row = 0; row < rows; row++) {
+            String[] numsInput = reader.readLine().split(" ");
+            for (int col = 0; col < cols; col++) {
+                matrix[row][col] = Integer.parseInt(numsInput[col]);
+            }
+        }
+        return matrix;
     }
 }
